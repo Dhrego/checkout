899dea7001a699c816ab8f76654634c1538a1a1e
Andrea <andrea.dick3@gmail.com>
2023-08-01 18:38:01 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <body className="container-fluid vh-100 d-flex jusfify-content-center align-items-center">
+        <div className="container col-10 text-center p-5 bg-white">
+          <nav class="navbar">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">
+                <i class="bi bi-arrow-left text-secondary"></i>
+              </a>
+              <p className="m-0 fs-4">Subscribe to Creator</p>
+              <a class="navbar-brand" href="#">
+                <i class="bi bi-x-lg text-secondary"></i>
+              </a>
+            </div>
+          </nav>
+        </div>
+      </body>
+    </>
+  );
+}
+
+export default App;
